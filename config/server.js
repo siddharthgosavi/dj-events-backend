03644d8030dc3641367f81f9b23e913b68e563c4
Siddharth <siddhugosavi89@gmail.com>
2023-08-29 13:54:32 +0530

    all backend changes

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e52ed7fe658aee15ca1fbebe40efab04'),
+    },
   },
 });
